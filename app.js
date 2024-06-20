139920f92ed0d0fdafd8676968d53b18a4899e6c
Brandon Koehler <z3nrider@gmail.com>
2024-06-20 19:44:58 -0400

    Add basic todo card and styling

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(__dirname + ""));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
