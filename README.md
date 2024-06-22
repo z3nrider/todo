cf25e5a3a249ca7affb90fab62a53571b8b497ca
Brandon Koehler <z3nrider@gmail.com>
2024-06-21 20:45:05 -0400

    connect to mongodb

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A simple todo application
 # development
 
 1. npm install express --save
-2. npm i bootstrap ejs nodemon
+2. npm i bootstrap ejs nodemon mongodb mongoose
--- app.js
+++ app.js
@@ -2,13 +2,57 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const mongoose = require('mongoose');
+const Todo = require('./models/todos');
+const dbURI = "mongodb+srv://bkoehler2012:H32ff9iXk5ef7KyO@cluster0.y5ex7mx.mongodb.net/todoapp?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to db and start server if successful
+mongoose.connect(dbURI)
+    .then ((result) => {
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`)
+        })
+    })
+    .catch((err) => { console.log(err) });
 
 app.use(express.static(__dirname + ""));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
-})
+});
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+app.get('/create-todo', (req, res) => {
+    const todo = new Todo({
+        task: 'mow the yard',
+        dueDate: 'june 23rd',
+    });
+    todo.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.get('/all-todos', (req, res) => {
+    Todo.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.get('/single-todo', (req, res) => {
+    Todo.findById('667619f4c02c2c89076b1b13')
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+
